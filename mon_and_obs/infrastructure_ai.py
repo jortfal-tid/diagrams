c11cb2819e699056daac97efbb3dca4b4c4bdf6c
José Manuel Ortega Falcón <e.nocnovum9.tid@telefonica.com>
2024-11-12 15:54:09 +0000

    add ai infrastructure diagram

---
+++ mon_and_obs/infrastructure_ai.py
@@ -0,0 +1,49 @@
+from diagrams import Cluster, Diagram
+
+from diagrams.k8s.infra import Node
+
+from diagrams.azure.compute import KubernetesServices
+
+from diagrams.generic.blank import Blank
+from diagrams.custom import Custom
+
+
+with Diagram("Infrastructure Architecture >> AI to Observability", direction="TB", filename="mon_and_obs_infrastucture_ai", show=False):
+    
+    with Cluster("Control Plane"):
+      aks = KubernetesServices("AKS")
+
+    with Cluster("Node Pool AI"):
+      
+      node_ai01 = Node("NODE AI01")
+      node_ai02 = Node("NODE AI02")
+      node_ai03 = Node("NODE AI03")
+
+      nodes_ai = [node_ai01, node_ai02, node_ai03]
+
+    with Cluster("NODE AI01 | Standard_NC16as_T4_v3​"):
+
+      aks_node_ai01 = KubernetesServices("Node AI01​")
+      ai_01 = Custom("AI Models", "./assets/img/ai-logo.png")
+
+    with Cluster("NODE AI02 | Standard_NC16as_T4_v3​"):
+
+      aks_node_ai02 = KubernetesServices("Node AI02​")
+      ai_02 = Custom("AI Models", "./assets/img/ai-logo.png")
+
+    with Cluster("NODE AI03 | Standard_NC16as_T4_v3​"):
+
+      aks_node_ai03 = KubernetesServices("Node AI03​")
+      ai_03 = Custom("AI Models", "./assets/img/ai-logo.png")
+
+    # Cluster
+    aks >> nodes_ai
+
+    # Node AI01
+    node_ai01 >> aks_node_ai01
+
+    # Node AI02
+    node_ai02 >> aks_node_ai02
+
+    # Node AIL03
+    node_ai03 >> aks_node_ai03
